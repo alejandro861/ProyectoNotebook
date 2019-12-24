cb59281d8ba3d6a210ff96c90a55f15757d6d252
webb1001 <58349946+webb1001@users.noreply.github.com>
2019-12-23 23:06:33 -0600

    Login

--- ProyectoNotebook/Form1.Designer.cs
+++ ProyectoNotebook/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ProyectoNotebook
 {
-    partial class Form1
+    partial class LoginForm
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -29,12 +29,95 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.UsuarioLabel = new System.Windows.Forms.Label();
+            this.ContraseñaLabel = new System.Windows.Forms.Label();
+            this.UsuarioTextBox = new System.Windows.Forms.TextBox();
+            this.ContraseñaTextBox = new System.Windows.Forms.TextBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.IngresarButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // UsuarioLabel
+            // 
+            this.UsuarioLabel.AutoSize = true;
+            this.UsuarioLabel.BackColor = System.Drawing.SystemColors.Control;
+            this.UsuarioLabel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UsuarioLabel.Location = new System.Drawing.Point(166, 208);
+            this.UsuarioLabel.Name = "UsuarioLabel";
+            this.UsuarioLabel.Size = new System.Drawing.Size(66, 18);
+            this.UsuarioLabel.TabIndex = 0;
+            this.UsuarioLabel.Text = "Usuario:";
+            // 
+            // ContraseñaLabel
+            // 
+            this.ContraseñaLabel.AutoSize = true;
+            this.ContraseñaLabel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ContraseñaLabel.Location = new System.Drawing.Point(166, 260);
+            this.ContraseñaLabel.Name = "ContraseñaLabel";
+            this.ContraseñaLabel.Size = new System.Drawing.Size(93, 18);
+            this.ContraseñaLabel.TabIndex = 1;
+            this.ContraseñaLabel.Text = "Contraseña:";
+            // 
+            // UsuarioTextBox
+            // 
+            this.UsuarioTextBox.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UsuarioTextBox.Location = new System.Drawing.Point(287, 205);
+            this.UsuarioTextBox.Name = "UsuarioTextBox";
+            this.UsuarioTextBox.Size = new System.Drawing.Size(185, 26);
+            this.UsuarioTextBox.TabIndex = 2;
+            // 
+            // ContraseñaTextBox
+            // 
+            this.ContraseñaTextBox.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ContraseñaTextBox.Location = new System.Drawing.Point(287, 257);
+            this.ContraseñaTextBox.Name = "ContraseñaTextBox";
+            this.ContraseñaTextBox.Size = new System.Drawing.Size(185, 26);
+            this.ContraseñaTextBox.TabIndex = 3;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // IngresarButton
+            // 
+            this.IngresarButton.Location = new System.Drawing.Point(299, 362);
+            this.IngresarButton.Name = "IngresarButton";
+            this.IngresarButton.Size = new System.Drawing.Size(75, 23);
+            this.IngresarButton.TabIndex = 4;
+            this.IngresarButton.Text = "Ingresar";
+            this.IngresarButton.UseVisualStyleBackColor = true;
+            this.IngresarButton.Click += new System.EventHandler(this.IngresarButton_Click);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackgroundImage = global::ProyectoNotebook.Properties.Resources.color_fondo_azul_claro_brillante_color_azul_claro_degradado_40417_157;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(692, 397);
+            this.Controls.Add(this.IngresarButton);
+            this.Controls.Add(this.ContraseñaTextBox);
+            this.Controls.Add(this.UsuarioTextBox);
+            this.Controls.Add(this.ContraseñaLabel);
+            this.Controls.Add(this.UsuarioLabel);
+            this.Name = "LoginForm";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label UsuarioLabel;
+        private System.Windows.Forms.Label ContraseñaLabel;
+        private System.Windows.Forms.TextBox UsuarioTextBox;
+        private System.Windows.Forms.TextBox ContraseñaTextBox;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Button IngresarButton;
     }
 }
 
--- ProyectoNotebook/Form1.cs
+++ ProyectoNotebook/Form1.cs
@@ -10,11 +10,48 @@ using System.Windows.Forms;
 
 namespace ProyectoNotebook
 {
-    public partial class Form1 : Form
+    public partial class LoginForm : System.Windows.Forms.Form
     {
-        public Form1()
+        public LoginForm()
         {
             InitializeComponent();
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+            UsuarioLabel.BackColor = Color.Transparent;
+            ContraseñaLabel.BackColor = Color.Transparent;
+        }
+        private bool InformacionEsValida()
+        {
+
+            bool esValida = true;
+            if(UsuarioTextBox.Text.Length == 0)
+            {
+                esValida = false;
+                errorProvider1.SetError(UsuarioTextBox, "Debe ingresar un usuario");
+            }
+            if (UsuarioTextBox.Text.Length == 0)
+            {
+                esValida = false;
+                errorProvider1.SetError(UsuarioTextBox, "Debe ingresar una contaseña");
+            }
+            return esValida;
+        }
+        private void LimpiarErrorProviders()
+        {
+            errorProvider1.SetError(UsuarioTextBox, "");
+            errorProvider1.SetError(ContraseñaTextBox, ""); 
+        }
+
+        private void IngresarButton_Click(object sender, EventArgs e)
+        {
+            if (InformacionEsValida())
+            {
+                //Faltaria autenticar al usuario
+                Hide();
+                Form2 PonerNombre = new Form2();
+            }
         }
     }
 }
